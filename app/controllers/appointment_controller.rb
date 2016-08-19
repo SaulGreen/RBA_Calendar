c7e60693cb5b9521327c6a5cc2606dca6f77976e
Saul Green <saulgreen7@hotmail.com>
2016-08-18 17:26:06 -0700

    Fix 5

--- app/controllers/appointment_controller.rb
+++ app/controllers/appointment_controller.rb
@@ -58,6 +58,7 @@ class AppointmentController < ApplicationController
   def nuevaCita
       @appointment = Appointment.new(appointment_params);
       @cliente = Client.where(:id => :client_id)
+      clienteDst = Client.where(:id => :client_id)
 
       fecha = @appointment.fecha
       hora = @appointment.hora
@@ -66,7 +67,7 @@ class AppointmentController < ApplicationController
       @appCheck = Appointment.where("fecha = ? AND hora = ? AND user_id = ? AND status_app = 1", fecha, hora, user).count
 
       if @appCheck > 0
-          @cliente.destroy
+          clienteDst.destroy
 
           respond_to do |format|
                format.json { render :text => @appCheck }
